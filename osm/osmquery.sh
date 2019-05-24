de703cba0661fad1a4c71b25f71b0087f136ce23
Dhirendra Singh <dhi.singh@gmail.com>
2019-05-24 14:37:22 +1000

    Saving OSM query results as GeoJSON (not CSV) as some results will be polygons

--- osm/osmquery.sh
+++ osm/osmquery.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+OSM2GEO=osmtogeojson
+command -v $OSM2GEO >/dev/null 2>&1 || { echo >&2 "Install $OSM2GEO first: npm install -g osmtogeojson"; exit 1; }
 
 if [ $# -ne 1 ];
     then echo "usage $0 FILE.query"
     exit
 fi
-CMD="wget -O $1.result.csv --post-file=$1 'http://overpass-api.de/api/interpreter'"
+CMD="wget -O .osm --post-file=$1 'http://overpass-api.de/api/interpreter'"
+echo $CMD; eval $CMD
+CMD="$OSM2GEO .osm > $1.result.json; rm .osm"
 echo $CMD; eval $CMD
