1f7cc1e88290f00e2cb4916a4c3be1251d1f49eb
Dhirendra Singh <dhi.singh@gmail.com>
2020-07-09 15:28:11 +1000

    MSC fires GeoJsons in relevant EPSG

--- mount-alexander-shire/phoenix-shapefiles/20181109/Evac_Phoenix_runs/msc-fires-to-json.sh
+++ mount-alexander-shire/phoenix-shapefiles/20181109/Evac_Phoenix_runs/msc-fires-to-json.sh
@@ -2,7 +2,7 @@
 
 dir=$(dirname "$0")
 
-for epsg in 28355 3857 ; do
+for epsg in 28355 3857 4326 ; do
         for file in $dir/*/*_grid.shp; do
                 cmd="ogr2ogr -f 'GeoJson' -t_srs 'EPSG:$epsg' $file.epsg$epsg.json $file"
                 echo $cmd; eval $cmd
